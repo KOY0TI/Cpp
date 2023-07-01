80196690ebb6e73a7d7f1e6c39fa3f141b7d38f0
KOY0TI <117745938+KOY0TI@users.noreply.github.com>
2023-07-01 13:35:31 +0200

    Create KandydaturaPrezydencka.cpp

---
+++ KandydaturaPrezydencka.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+
+
+int wiek;
+
+
+
+int main()
+
+{
+
+    cout << "Ile masz lat: ";
+
+    cin >> wiek;
+
+
+
+    if (wiek<18)
+
+    {
+
+        cout<<"Nie jestes pelnoletni i nie mozesz zostac prezydentem";
+
+    }
+
+    else if ((wiek>=18) && (wiek<35))
+
+    {
+
+        cout<<"Jestes pelnoletni ale nie mozesz zostac prezydentem";
+
+    }
+
+    else
+
+    {
+
+        cout<<"Jestes pelnoletni i mozesz zostac prezydentem";
+
+    }
+
+    return 0;
+
